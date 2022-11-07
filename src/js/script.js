df3c6d3e544b5494f0aa69b6afbe7aaded91bc0c
bondarkoff <yehor.bondarkoff@gmail.com>
2022-11-07 15:32:18 +0200

    _faq-accordion

--- src/js/script.js
+++ src/js/script.js
@@ -1,22 +1,43 @@
 "use strict";
 
-const accordionBtns = document.querySelectorAll(".hamburger"),
-      text = document.querySelectorAll(".faq__item-text"),
-      span = document.querySelector(".span");
+// const accordionBtns = document.querySelectorAll(".hamburger"),
+//       text = document.querySelectorAll(".faq__item-text"),
+//       span = document.querySelector(".span");
+
+// accordionBtns.forEach((accordion) => {
+//   accordion.onclick = function () {
+//     this.classList.toggle("active");
+
+//     span.classList.toggle("active");
+    
+//     let content = this.nextElementSibling;
+//     console.log(content);
+
+//     content.classList.toggle("active");
+
+//   };
+// });
+
+const accordionBtns = document.querySelectorAll(".faq__item-header");
 
 accordionBtns.forEach((accordion) => {
   accordion.onclick = function () {
     this.classList.toggle("active");
 
-    span.classList.toggle("active");
-    
     let content = this.nextElementSibling;
     console.log(content);
 
-    content.classList.toggle("active");
-
+    if (content.style.maxHeight) {
+      //this is if the accordion is open
+      content.style.maxHeight = null;
+    } else {
+      //if the accordion is currently closed
+      content.style.maxHeight = content.scrollHeight + "px";
+      console.log(content.style.maxHeight);
+    }
   };
 });
+
 
 
 
